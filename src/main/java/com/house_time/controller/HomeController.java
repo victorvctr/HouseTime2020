2eea4a2d96fdff879abaa9f57258dd9d6c0534ec
victo <victo@DESKTOP-T2O9ICU.home>
2020-03-03 23:41:30 -0300

    iniciando projeto -Front-End

---
+++ src/main/java/com/house_time/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.house_time.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+	@Controller
+
+	public class HomeController {
+		@RequestMapping(method=RequestMethod.GET,value ="/iniciar")
+		public ModelAndView inicio() {
+			ModelAndView mv = new ModelAndView("home/index");
+			return mv;
+			
+		}
+	
+		
+		
+		
+
+}
---
+++ src/test/java/com/house_time/HouseTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
