358a7ccdec85371f4624f4de8f47b6123cfee36b
victor <victorvctr1@homail.com>
2020-06-12 11:36:28 -0300

    Configuração no Produtocontroller para cadastrar imagem 12/06/2020

--- src/main/java/com/house_time/controller/ProdutoController.java
+++ src/main/java/com/house_time/controller/ProdutoController.java
@@ -1,5 +1,7 @@
 package com.house_time.controller;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 
 import com.house_time.models.Produto;
 
 import com.house_time.repositorios.ProdutoRepositorio;
+
+import com.house_time.infra.FotoService;
 
 @Controller
 @RequestMapping("/produto")
@@ -24,6 +29,9 @@ public class ProdutoController {
 
 	@Autowired
 	private ProdutoRepositorio produtos;
+
+	@Autowired
+	private FotoService fotoService;
 
 	@GetMapping
 	public ModelAndView listar() {
@@ -44,13 +52,13 @@ public class ProdutoController {
 
 		return modelAndView;
 	}
-	
+
 	@GetMapping("editar/{id}")
 	public ModelAndView editar(@PathVariable Long id) {
 
 		return novo(produtos.getOne(id));
 	}
-	
+
 	@PostMapping(value = "excluir/{id}")
 	public String excluir(@PathVariable Long id, RedirectAttributes attributes) {
 
@@ -60,50 +68,72 @@ public class ProdutoController {
 
 		return "redirect:/produto";
 	}
-	
-	
-	
-	@PostMapping("/salvar")
-	public ModelAndView Cadastrar(@Valid Produto produto, BindingResult result, RedirectAttributes attributes) {
 
-		
-		if (result.hasErrors()) { 
+	@PostMapping("/salvar")
+	public ModelAndView Cadastrar(@RequestParam("file") MultipartFile file, @Valid Produto produto,
+			BindingResult result, RedirectAttributes attributes) {
+
+		if (result.hasErrors()) {
 			System.out.println(result.getErrorCount());
 			System.out.println(result.getAllErrors());
-			return novo(produto); }
-		
+			return novo(produto);
+		}
+
+		if (!file.isEmpty()) {
+
+			if (produto.getUrl_imagem() != null && !produto.getUrl_imagem().isEmpty()) {
+				fotoService.removerFoto(produto.getUrl_imagem());
+			}
+
+			String arquivoFoto = fotoService.doUpload(file, produto);
+
+			if (arquivoFoto.equals("erro")) {
+				attributes.addFlashAttribute("mensagem", "Problemas para salvar a imagem do produto!!");
+				return novo(produto);
+			} else {
+				produto.setUrl_imagem(arquivoFoto);
+			}
+		}
 
 		produtos.save(produto);
 
 		attributes.addFlashAttribute("mensagem", "Produto salvo com sucesso!!");
 		return new ModelAndView("redirect:/produto");
-		
 
 	}
 	
-
 	
+	@GetMapping("/imagem/{nome:.*}")
+	public @ResponseBody byte[] recuperarFoto(@PathVariable String nome) throws IOException {
+		
+		return fotoService.recuperarFoto(nome);
+	}
+
 	@GetMapping("/apple")
 	public String apple() {
 		return "produto/apple";
 	}
-	
+
 	@GetMapping("/samsung")
 	public String sansung() {
 		return "produto/samsung";
 	}
+
 	@GetMapping("/diesel")
 	public String diesel() {
 		return "produto/diesel";
 	}
+
 	@GetMapping("/fossil")
 	public String fossil() {
 		return "produto/fossil";
 	}
+
 	@GetMapping("/montblanc")
 	public String montblanc() {
 		return "produto/montblanc";
 	}
+
 	@GetMapping("/xiaomi")
 	public String xiaomi() {
 		return "produto/xiaomi";
