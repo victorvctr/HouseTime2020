afe193c3aaab037c01fe2aebce6b2569a001613e
victo <victorvctr1@hotmail.com>
2020-06-17 21:36:56 -0300

    As fotos so aparecem para o administrador

--- src/main/java/com/house_time/models/Contato.java
+++ src/main/java/com/house_time/models/Contato.java
@@ -67,27 +67,6 @@ public class Contato implements Serializable{
 		return serialVersionUID;
 	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Contato other = (Contato) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
 	
 	}
 
