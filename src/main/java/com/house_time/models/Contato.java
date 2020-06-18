8109ecc387c343cb51b902ee18635f00cebc077e
victor <victorvctr1@homail.com>
2020-06-18 13:15:52 -0300

    testando

--- src/main/java/com/house_time/models/Contato.java
+++ src/main/java/com/house_time/models/Contato.java
@@ -13,17 +13,18 @@ public class Contato implements Serializable{
 	private static final long serialVersionUID = -309513637403441918L;
 	
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	private Long id;
 	private String nome;
 	private String sobrenome;
 	private String celular;
 	private String email;
 	private String assunto;
 	private String mensagem;
-	public int getId() {
+	
+	public Long getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getNome() {
